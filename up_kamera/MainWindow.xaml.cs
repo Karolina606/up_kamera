08469c58bf580795814076e6d3d810854114f41b
karolina <nogacka.karolina@gmail.com>
2021-12-22 14:16:06 +0100

    Dodanie zmian zmian z zajec

--- up_kamera/MainWindow.xaml.cs
+++ up_kamera/MainWindow.xaml.cs
@@ -25,6 +25,9 @@ using AForge.Controls;
 using Accord.Video.FFMPEG;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Threading;
+using Accord.Vision.Motion;
+using System.Media;
 
 namespace up_kamera
 {
@@ -43,6 +46,19 @@ namespace up_kamera
         private VideoSourcePlayer videoSourcePlayer;
         private VideoFileWriter videoFileWriter;
         private bool isRecording = false;
+        private bool isLookingForChanges = false;
+        private int Contrast = 0;
+        Bitmap frameToDetect;
+
+        // Recording params
+        int width = 1280;
+        int height = 720;
+        int fps = 9;
+
+        // Motion detector
+        MotionDetector detector = new MotionDetector(
+            new SimpleBackgroundModelingDetector(),
+            new MotionAreaHighlighting());
 
         public static List<string> GetAllConnectedCameras()
         {
@@ -78,7 +94,7 @@ namespace up_kamera
 
             Console.WriteLine("Wybrałeś kamerę");
 
-            //parametry
+            // Parametry
             int zoom, exposure, pan, tilt, roll, iris, focus;
             CameraControlFlags flag;
             videoCaptureDevice.GetCameraProperty(CameraControlProperty.Zoom, out zoom, out flag);
@@ -93,9 +109,12 @@ namespace up_kamera
             tbPan.Text = pan.ToString();
             tbTilt.Text = tilt.ToString();
             tbRoll.Text = roll.ToString();
-            tbIris.Text = iris.ToString();
+            //tbIris.Text = iris.ToString();
             tbFocus.Text = focus.ToString();
+            tbContrast.Text = "0";
 
+
+            // Sprawdzenie zakresów parametrów
             int zoom2, exposure2, pan2, tilt2, roll2, iris2, focus2, non;
             int zoom3, exposure3, pan3, tilt3, roll3, iris3, focus3;
             String param = "Parameters: \n";
@@ -115,17 +134,63 @@ namespace up_kamera
             param += $"Roll min: {roll} max: {roll2} step: {roll3} \n";
             param += $"Focus min: {focus} max: {focus2} step: {focus3} \n";
 
+
+            // Recording params
+            tbRecordingWidth.Text = width.ToString();
+            tbRecordingHeight.Text = height.ToString();
+            tbRecordingFps.Text = fps.ToString();
+
             Console.WriteLine(param);
         }
 
         private void videoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            pictBox.Image = (Bitmap) eventArgs.Frame.Clone();
+            // Kontrast
+            ContrastCorrection filter = new ContrastCorrection(Contrast);
+            Bitmap original = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap toFrame = new Bitmap(original, new System.Drawing.Size(original.Width/2, original.Height/2));
+            pictBox.Image = filter.Apply(toFrame);
 
             if (isRecording)
             {
-                Bitmap second = (Bitmap)eventArgs.Frame.Clone();
+                Bitmap second = filter.Apply((Bitmap)eventArgs.Frame.Clone());
                 videoFileWriter.WriteVideoFrame(second);
+            }
+
+            try
+            {
+                if (isLookingForChanges)
+                {
+                    frameToDetect = (Bitmap) toFrame.Clone();
+                    detectMotion();
+                }
+            }catch (System.Exception e)
+            {
+                
+                lblDetectInfo.Content = "Wykryto ruch";
+                //Thread.Sleep(2000);
+            }
+        }
+
+        private void detectMotion()
+        {
+            try
+            {
+                float motionFractal = detector.ProcessFrame(frameToDetect);
+                if (motionFractal > 0.2)
+                {
+
+                    Console.WriteLine("Wykryto ruch");
+                    SystemSounds.Beep.Play();
+                    // lblInfo.Content = "Wykryto ruch";
+                    // Thread.Sleep(2000);
+                }
+            }
+            catch (System.Exception e)
+            {
+                
+                lblDetectInfo.Content = "Wykryto ruch";
+                //Thread.Sleep(2000);
             }
         }
 
@@ -136,13 +201,20 @@ namespace up_kamera
                 //Save First
                 Bitmap varBmp = new Bitmap(pictBox.Image);
                 Bitmap newBitmap = new Bitmap(varBmp);
-                varBmp.Save(@"zdjecie.png", ImageFormat.Png);
+                varBmp.Save(@"zdjecie.jpg", ImageFormat.Jpeg);
                 //Now Dispose to free the memory
                 varBmp.Dispose();
                 varBmp = null;
+
+                // Wyświetlenie informacji 
+                lblInfo.Content = "Zdjęcie zostało zrobione!";
             }
             else
-            { MessageBox.Show("null exception"); }
+            {
+                MessageBox.Show("null exception");
+                // Wyświetlenie informacji 
+                lblInfo.Content = "Zdjęcie nie zostało zrobione!";
+            }
         }
 
         private void windowClosing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -159,9 +231,19 @@ namespace up_kamera
             videoCaptureDevice.SetCameraProperty(CameraControlProperty.Exposure, Int32.Parse(tbExposure.Text), CameraControlFlags.Manual);
             videoCaptureDevice.SetCameraProperty(CameraControlProperty.Pan, Int32.Parse(tbPan.Text), CameraControlFlags.Manual);
             videoCaptureDevice.SetCameraProperty(CameraControlProperty.Tilt, Int32.Parse(tbTilt.Text), CameraControlFlags.Manual);
-            videoCaptureDevice.SetCameraProperty(CameraControlProperty.Iris, Int32.Parse(tbIris.Text), CameraControlFlags.Manual);
+            //videoCaptureDevice.SetCameraProperty(CameraControlProperty.Iris, Int32.Parse(tbIris.Text), CameraControlFlags.Manual);
             videoCaptureDevice.SetCameraProperty(CameraControlProperty.Roll, Int32.Parse(tbRoll.Text), CameraControlFlags.Manual);
             videoCaptureDevice.SetCameraProperty(CameraControlProperty.Focus, Int32.Parse(tbFocus.Text), CameraControlFlags.Manual);
+
+            Contrast = Int32.Parse(tbContrast.Text);
+
+            // Recording params
+            width = Int32.Parse(tbRecordingWidth.Text);
+            height = Int32.Parse(tbRecordingHeight.Text);
+            fps = Int32.Parse(tbRecordingFps.Text);
+
+            // Wyświetlenie informacji 
+            lblInfo.Content = "Ustawienia zmienione!";
         }
 
         private void btnStartRecordingClick(object sender, RoutedEventArgs e)
@@ -173,16 +255,16 @@ namespace up_kamera
             videoSourcePlayer.Start();
             videoFileWriter = new VideoFileWriter();
 
-            int width = 1280;
-            int height = 720;
-            int fps = 9;
 
             if(videoSourcePlayer.IsRunning && videoCaptureDevice.IsRunning)
             {
                 videoFileWriter.Open("nagranie.wmv", width, height, fps, VideoCodec.WMV1);
                 //videoFileWriter.WriteVideoFrame((Bitmap)pictBox.Image);
             }
-            
+
+            // Wyświetlenie informacji 
+            lblInfo.Content = "Nagrywanie rozpoczęte!";
+
             isRecording = true;
 
         }
@@ -194,6 +276,23 @@ namespace up_kamera
             isRecording = false;
             videoSourcePlayer.Stop();
             videoFileWriter.Close();
+
+            // Wyświetlenie informacji
+            lblInfo.Content = "Nagrywanie zakończone!";
+        }
+
+        private void btnDetectionOffClick(object sender, RoutedEventArgs e)
+        {
+            isLookingForChanges = false;
+            btnDetectionOff.IsEnabled = false;
+            btnDetectionOn.IsEnabled = true;
+        }
+
+        private void btnDetectionOnClick(object sender, RoutedEventArgs e)
+        {
+            isLookingForChanges = true;
+            btnDetectionOff.IsEnabled = true;
+            btnDetectionOn.IsEnabled = false;
         }
     }
 }
